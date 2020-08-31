ef3e619884aab692a5302c001d0fdf34891a4e3b
Bruno Mota <bmota2233@gmail.com>
2020-08-30 23:17:26 -0400

    Updates

---
+++ precache-manifest.269a00c5f64bac2a809a3644baa41b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68f787a22a41597bb94d70575eb0ced",
+    "url": "/BrunoBMota/laptop_customizer_bruno/index.html"
+  },
+  {
+    "revision": "d62a71a8cb0de37b5cfd",
+    "url": "/BrunoBMota/laptop_customizer_bruno/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/BrunoBMota/laptop_customizer_bruno/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "d62a71a8cb0de37b5cfd",
+    "url": "/BrunoBMota/laptop_customizer_bruno/static/js/main.54308427.chunk.js"
+  },
+  {
+    "revision": "d335f046d22da69c9f59",
+    "url": "/BrunoBMota/laptop_customizer_bruno/static/js/runtime~main.b592d46e.js"
+  }
+]);
