0e81bae8146f40b3fd5e9de14f808144901f2700
bmota2233 <bmota2233@gmail.com>
2020-09-03 01:25:34 -0400

    refactor

--- src/MainSummary.js
+++ src/MainSummary.js
@@ -1,39 +1,7 @@
 import React, { Component } from 'react';
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
-
-
-function Summary(props){
-  const summary = Object.keys(props.selected).map((feature, idx) => {
-    const featureHash = feature + '-' + idx;
-    const selectedOption = props.selected[feature];
-
-    return (
-      <div className="summary__option" key={featureHash}>
-        <div className="summary__option__label">{feature} </div>
-        <div className="summary__option__value">{selectedOption.name}</div>
-        <div className="summary__option__cost">
-          {USCurrencyFormat.format(selectedOption.cost)}
-        </div>
-      </div>
-    );
-  });
-
-  return summary;
-}
-
-
-function SummaryTotal(props){
-  return (
-    <div className="summary__total">
-      <div className="summary__total__label">Total</div>
-      <div className="summary__total__value">{USCurrencyFormat.format(props.total)}</div>
-    </div>
-  )
-}
+import Summary from './Summary';
+import SummaryTotal from './SummaryTotal';
 
 class MainSummary extends Component{
   static defaultProps = {
